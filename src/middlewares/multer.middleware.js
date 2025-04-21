9313674ad98a8c8c663a2776b3450121aca77851
hasanbut17314 <hasanbut17314@gmail.com>
2025-04-21 23:32:27 +0500

    refactor: update multer middleware to use /tmp directory and remove temp directory creation

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -3,15 +3,9 @@ import { v4 as uuidv4 } from "uuid";
 import path from "path";
 import fs from "fs";
 
-// Create the temp directory if it doesn't exist
-const tempDir = "./public/temp";
-if (!fs.existsSync(tempDir)) {
-    fs.mkdirSync(tempDir, { recursive: true });
-}
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, tempDir);
+        cb(null, /tmp/); // Save files to the tmp directory
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = `${uuidv4()}-${Date.now()}`;
